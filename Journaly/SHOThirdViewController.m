8bfbe4581d6b312525781427468e2956384c7dfb
shalynnho <shalynkh@usc.edu>
2014-05-07 17:17:21 -0700

    added third tab bar item + controller with nav bar and sign out.

---
+++ Journaly/SHOThirdViewController.m
@@ -0,0 +1,38 @@
+//
+//  SHOThirdViewController.m
+//  Journaly
+//
+//  Created by Derp Derp on 5/7/14.
+//  Copyright (c) 2014 SHO. All rights reserved.
+//
+
+#import "SHOThirdViewController.h"
+
+@interface SHOThirdViewController ()
+
+@end
+
+@implementation SHOThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
